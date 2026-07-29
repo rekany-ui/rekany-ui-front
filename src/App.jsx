3b6881eee96e671fe99df568227b69468d36d0bd
RoccoRK119 <rkrocco119@gmail.com>
2026-07-29 19:20:29 +0300

    résolution des erreurs build

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 // src/App.tsx
 
+import { lazy, Suspense } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router-dom";
 import { queryClient } from "./config/queryClient";
 import Layout from "./components/Layout";
 import NotFound404 from "./components/NotFound404";
-import HomePage from "./pages/Home/Home";
-import ContactPage from "./pages/Contact/ContactPage";
-import AboutPage from "./pages/About/AboutPage";
-import ProductsPage from "./pages/Products/ProductsPage";
+
+const HomePage = lazy(() => import("./pages/Home/Home"));
+const ContactPage = lazy(() => import("./pages/Contact/ContactPage"));
+const AboutPage = lazy(() => import("./pages/About/AboutPage"));
+const ProductsPage = lazy(() => import("./pages/Products/ProductsPage"));
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/a-propos" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/produits" element={<ProductsPage />} />
-        </Route>
+      <Suspense fallback={<div>Chargement...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/a-propos" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/produits" element={<ProductsPage />} />
+          </Route>
 
-        <Route path="*" element={<NotFound404 />} />
-      </Routes>
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </Suspense>
     </QueryClientProvider>
   );
 }
